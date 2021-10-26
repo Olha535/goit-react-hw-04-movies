ede3c58f73935576fd6a78f65689d6559b834729
Olha Tarasova <tarasovao0505@gmail.com>
2021-10-26 10:01:51 +0300

    Added started filed

---
+++ src/component/App/App.js
@@ -0,0 +1,12 @@
+// import './App.css';
+import Container from '../Container';
+
+function App() {
+  return (
+    <Container className="App">
+      <header className="App-header"></header>
+    </Container>
+  );
+}
+
+export default App;
